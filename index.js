e0234f3eb7d46e2abd460d8ff6164308ceb58376
Joseph <josephylee72@gmail.com>
2023-09-24 21:15:28 -0700

    completed array filter lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(collection, string) {
+  return collection.filter(name => name.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(collection, string) {
+  return collection.filter(name => name.startsWith(string))
+}
+
+function matchName(collection, string) {
+  return collection.filter(obj => obj.name === string)
+}
